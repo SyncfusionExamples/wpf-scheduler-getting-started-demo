9b99e35c21eb8d75a3e425cccaf5a6f849f8fdb1
PiruthivirajM <44426156+PiruthivirajM@users.noreply.github.com>
2020-07-02 18:36:26 +0530

    sample files are added

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.UI.Xaml.Scheduler;
+using System;
+using System.Windows;
+
+namespace GettingStarted
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            ScheduleAppointmentCollection appointmentCollection = new ScheduleAppointmentCollection();
+            //Creating new event   
+            ScheduleAppointment clientMeeting = new ScheduleAppointment();
+            DateTime currentDate = DateTime.Now;
+            DateTime startTime = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, 10, 0, 0);
+            DateTime endTime = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, 12, 0, 0);
+            clientMeeting.StartTime = startTime;
+            clientMeeting.EndTime = endTime;
+            clientMeeting.Subject = "ClientMeeting";
+            appointmentCollection.Add(clientMeeting);
+            Schedule.ItemsSource = appointmentCollection;
+
+        }
+    }
+}
+
